f35264797c42587621a6d6203e6bd5c2f3d16ef7
Andrey Chernoprudov <dinloq@gmail.com>
2018-06-17 12:59:06 +0500

    animate appear/disappear

--- InspectFile/View/InspectFileViewController.swift
+++ InspectFile/View/InspectFileViewController.swift
@@ -20,10 +20,16 @@ class InspectFileViewController: UIViewController, InspectFileView {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.view.isHidden = true
         setupTableView()
         setupDependencies()
         
         viewModel?.start()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animateAppearing()
     }
     
     func updateData() {
@@ -32,18 +38,36 @@ class InspectFileViewController: UIViewController, InspectFileView {
     
     func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 
     @IBAction func closeController(_ sender: Any) {
-        extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        let height = navigationController!.view.frame.size.height
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.navigationController!.view.transform
+                = CGAffineTransform(translationX: 0, y: height)
+        }, completion: { _ in
+            self.extensionContext!.completeRequest(
+                returningItems: [],
+                completionHandler: nil)
+        })
     }
     
     // MARK: - Private
     
-    func setupDependencies() {
+    private func setupDependencies() {
         let assembler = InspectFileAssembler(extensionContext: extensionContext!)
         viewModel = assembler.buildModel(for: self)
+    }
+    
+    private func animateAppearing() {
+        let view = navigationController!.view!
+        view.isHidden = false
+        view.transform = CGAffineTransform(translationX: 0, y: view.frame.size.height)
+        UIView.animate(withDuration: 0.25) {
+            view.transform = .identity
+        }
     }
 }
